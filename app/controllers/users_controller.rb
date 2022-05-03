f9e58b8bd6aa9db30437723fd3ad52a9db46faf7
afda1021 <63727686+afda1021@users.noreply.github.com>
2022-05-03 13:26:34 +0900

    user作成、一覧表示の仮実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit(:name))
+    if @user.save
+      redirect_to users_index_url
+    else
+      render 'new'
+    end
+  end
+end
