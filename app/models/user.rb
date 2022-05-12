215a06adf7827735d4fc6b508d75a63e66cfdef6
yuki hara <hara.0509@icloud.com>
2022-05-12 23:56:43 +0900

    関連づけ追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :user_groups, dependent: :destroy
+  has_many :groups, through: :user_groups
+
   validates :name, presence: true, length: { maximum: 20 }
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
