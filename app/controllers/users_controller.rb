4b97bd17d8b64cc74eb3708baea407a6a20afe25
afda1021 <63727686+afda1021@users.noreply.github.com>
2022-05-15 19:27:59 +0900

    password保存処理の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params.require(:user).permit(:name, :email))
+    @user = User.new(params.require(:user).permit(:name, :email, :password, :password_confirmation))
     if @user.save
       redirect_to users_url
     else
