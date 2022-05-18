9218d88660c06971917c52abaa48d4511bb7a26e
afda1021 <63727686+afda1021@users.noreply.github.com>
2022-05-18 21:02:45 +0900

    ログイン処理の実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to users_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'sessions/new'
   post 'sessions/create'
+  delete 'sessions/destroy'
   root 'static_pages#home'
   resources :users
   resources :groups, only: %i[index new create]
