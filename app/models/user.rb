147240f9d9eacb8e2b0b54e999d3b12f59482ed0
afda1021 <63727686+afda1021@users.noreply.github.com>
2022-05-28 11:27:56 +0900

    passwordにvalidationをかけたときのgroup作成エラーをTODOに残した

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 20 }
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  # TODO: passwordのvalidationエラーを解決
+  # validates :password, presence: true, length: { minimum: 6 }
 end
