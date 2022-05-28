de8eee01014b7a79345ccda21c72841f51433fc0
afda1021 <63727686+afda1021@users.noreply.github.com>
2022-05-28 11:53:31 +0900

    user_paramsをprivateメソッドに定義、viewの整理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params.require(:user).permit(:name, :email, :password, :password_confirmation))
+    @user = User.new(user_params)
     if @user.save
       redirect_to users_url
     else
@@ -16,4 +16,10 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
