b87da16b0027ceccf5f94492667b901336fa0641
afda1021 <63727686+afda1021@users.noreply.github.com>
2022-06-04 17:40:22 +0900

    友達承認機能のモデル実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   has_many :groups, through: :user_groups
   
   has_many :active_friendship, class_name: "Friendship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_friendship, class_name: "Friendship", foreign_key: "followed_id", dependent: :destroy
   has_many :following, through: :active_friendship, source: :followed
+  has_many :followers, through: :passive_friendship, source: :follower
 
   validates :name, presence: true, length: { maximum: 20 }
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
