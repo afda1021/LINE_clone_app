4763121c8dd5caf41e455bef6684a27568ba948b
afda1021 <63727686+afda1021@users.noreply.github.com>
2022-06-04 17:31:41 +0900

    友達申請機能のモデル実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_many :user_groups, dependent: :destroy
   has_many :groups, through: :user_groups
+  
+  has_many :active_friendship, class_name: "Friendship", foreign_key: "follower_id", dependent: :destroy
+  has_many :following, through: :active_friendship, source: :followed
 
   validates :name, presence: true, length: { maximum: 20 }
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
