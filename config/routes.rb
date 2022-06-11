b514b893f90a309af7dae7b1441f2f8feefd7329
afda1021 <63727686+afda1021@users.noreply.github.com>
2022-06-11 15:39:02 +0900

    リクエスト承認機能の実装、relationships_controllerはusers_controllerにネスト

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :users do
     get :search, on: :collection
     post :search, on: :collection
+    resources :friendships, only: %i[create] 
   end
-  resources :friendships, only: %i[create]
   resources :groups, only: %i[index new create]
 end
